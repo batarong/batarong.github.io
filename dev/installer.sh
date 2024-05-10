fcfb8f381b55b406969f737d194a79112a479447
sam <77495892+1jammer1@users.noreply.github.com>
2024-05-10 16:13:06 -0400

    Update installer.sh
    Rewrite in python

--- dev/installer.sh
+++ dev/installer.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
-cd /tmp
-apt install git
-git clone https://github.com/batarong/Batarong-Installer.git
-cd Batarong-Installer
-echo "Enter your device path:"
-read cool
-bash install.sh -d $cool
+#!/bin/python3
+import os
+
+os.chdir("/tmp")
+# dependencys
+os.system("sudo apt install -y git")
+# grab the installer
+os.system("sudo git clone https://github.com/batarong/Batarong-Installer.git")
+# Open batarong
+os.chdir("/tmp/Batarong-Installer")
+# future proofing
+os.system("sudo chmod 777 install.sh")
+# Get the device path from user input
+device_path = input("Enter your device path: ")
+
+# Run the install.sh script with the device path using os.system()
+os.system(f"bash install.sh -d {device_path}")
