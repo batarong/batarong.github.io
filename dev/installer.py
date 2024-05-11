728a58923b3a819710724f2e83d03bc07f355bf4
sam <77495892+1jammer1@users.noreply.github.com>
2024-05-11 12:21:59 -0400

    do for all apt systems

--- dev/installer.py
+++ dev/installer.py
@@ -8,13 +8,13 @@ with open('/etc/os-release', 'r') as file:
             NAME = line.split('=')[1].strip().strip('"')
             break
           
-if NAME == "Debian GNU/Linux":
+if NAME == "Debian GNU/Linux" or NAME == "Ubuntu":
     # dependencies
     os.system("sudo apt install -y git sudo fdisk debootstrap dosfstools util-linux")
 else:
     print("You are not on Debian. This may not work. Check https://github.com/batarong/Batarong-Installer/wiki/Errors for more information.")
-    print("Continuing...")
-    time.sleep(2)
+    print("Continuing... Press Ctrl C now if your going to")
+    time.sleep(3)
     print("Going!!!")
 
 # Change directory
