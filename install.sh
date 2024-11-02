099190f39a952868dc044bed89688d863d447371
nile <77495892+1jammer1@users.noreply.github.com>
2024-11-02 15:02:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,5 +9,5 @@ deb http://deb.debian.org/debian/ stable-updates main contrib non-free non-free-
 deb-src http://deb.debian.org/debian/ stable-updates main contrib non-free non-free-firmware" | tee /etc/apt/sources.list
 dpkg --add-architecture i386
 apt update
-apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
-apt install steam-installer
+apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+apt install -y steam-installer
