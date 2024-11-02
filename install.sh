4b0be27ccabb1c745d0d5471552b7f83f8833764
nile <77495892+1jammer1@users.noreply.github.com>
2024-11-02 15:00:54 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,120 +1,13 @@
 #!/bin/bash
 
-
-while getopts ":b:d:h" opt; do
-  case ${opt} in
-    d )
-      device=$OPTARG
-      echo "Block device specified: $device"
-      # Add your logic for handling the block device here
-      if [ ! -d "/tmp/Batarong-Builder/rootfs" ]; then
-        echo "WARN: if you are not root ctrl-c and run with sudo"
-        echo "WARN: Please check that you have unmounted the disk if so ignore"
-        sleep 5
-        devicei=$OPTARG
-        devicei+="*"
-        sudo umount -f $devicei 2>/dev/null
-        sudo dd if=$device of=/dev/urandom count=2000
-        echo "type=83" | sudo sfdisk $device
-        num_list=(15 20 25 30 35 40 45 50 55 60 65 70 75 80 85 90 95 100 105 110 115 120 125 130)
-        deviced="${device: -3}"
-        result=()
-        for num in "${num_list[@]}"; do
-            device_size=$(lsblk -b -o NAME,SIZE /dev/"$deviced" | awk -v dev="$deviced" '$1==dev {print $2/1024/1024/1024}')
-            rounded_size=$(printf "%.0f" "$device_size")
-            result+=("$rounded_size")
-        done
-
-        echo $result
-        echo $device_size
-        echo $deviced
-        if [ $result -eq 0 ]; then
-          echo "your disk is small"
-          exit 0
-        fi
-        layouts="layout"
-        layouts+=$result
-        sudo sfdisk $device < layout/$layouts
-        mkdir /tmp/tmp
-        mkdir /tmp/tmp/mnt
-        device1=$OPTARG
-        device2=$OPTARG
-        device1+="1"
-        device2+="2"
-        echo "WARN: acsept any prompts"
-        sudo mkfs.ext4 -F $device1
-        sudo mkfs -t fat $device2
-        e2label $device1 "BIG-BATARONG"
-        dosfslabel $device2 "BATA-EFI"
-        sudo mount $device1 /tmp/tmp/mnt
-        sudo rsync -aAXHv / --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /tmp/tmp/mnt
-        mkdir /tmp/tmp/mnt/dev
-        mkdir /tmp/tmp/mnt/sys
-        sudo mount --rbind /sys /tmp/tmp/mnt/sys/
-        sudo mount --rbind /dev /tmp/tmp/mnt/dev/
-        sudo mkdir -p /tmp/tmp/mnt/boot/EFI
-        sudo mount $device2 /tmp/tmp/mnt/boot/EFI
-        sudo mkdir -p /tmp/tmp/mnt/boot/EFI/EFI
-        sudo mkdir -p /tmp/tmp/mnt/var/cache/apt/archives/partial
-        cd /tmp/tmp/mnt/boot/EFI/EFI
-        git clone https://github.com/batarong/refind.git
-        mv refind boot
-        cd /tmp/tmp/mnt/boot/
-        wget https://batarong.github.io/dev/refind_linuxi.conf -O refind_linux.conf
-        cd /root/Batarong-Installer/
-        bash /root/Batarong-Installer/config.sh
-        echo "root:batarong" | chpasswd -R /tmp/tmp/mnt
-        useradd -m -R /tmp/tmp/mnt -p $(openssl passwd -1 batarong) batarong
-        sudo chroot /tmp/tmp/mnt usermod --shell /bin/bash batarong
-        sudo chroot /tmp/tmp/mnt usermod --shell /bin/bash root
-        sudo chroot /tmp/tmp/mnt usermod -a -G sudo batarong 
-        echo "Done: Unmounting"
-        sudo umount $devicei 2>/dev/null
-      else
-        devicei=$OPTARG
-        devicei+="*"
-        sudo umount -f $devicei 2>/dev/null
-        sudo dd if=$device of=/dev/urandom count=2000
-        echo "type=83" | sudo sfdisk $device
-        sudo sfdisk $device < layout
-        mkdir /tmp/tmp
-        mkdir /tmp/tmp/mnt
-        device1=$OPTARG
-        device2=$OPTARG
-        device1+="1"
-        device2+="2"
-        echo "WARN: acsept any prompts"
-        sudo mkfs.ext4 $device1
-        sudo mkfs -t fat $device2
-        e2label $device1 "INSTALL-ROOT"
-        dosfslabel $device2 "INSTALL-EFI"
-        sudo mount $device1 /tmp/tmp/mnt
-        sudo cp -r rootfs/* /tmp/tmp/mnt
-        sudo mount --rbind /sys /tmp/tmp/mnt/sys/
-        sudo mount --rbind /dev /tmp/tmp/mnt/dev/
-        sudo mkdir /tmp/tmp/mnt/boot/EFI
-        sudo mount $device2 /tmp/tmp/mnt/boot/EFI
-        sudo mkdir /tmp/tmp/mnt/boot/EFI/EFI
-        cd /tmp/tmp/mnt/boot/EFI/EFI
-        git clone https://github.com/batarong/refind.git
-        cd /tmp/tmp/mnt/boot
-        wget https://batarong.github.io/dev/refind_linux.conf
-        cd /tmp/Bat*
-        bash config.sh
-        echo "root:batarong" | chpasswd -R /tmp/tmp/mnt
-        echo "Done: Unmounting"
-        sudo umount $devicei 2>/dev/null
-      fi
-      ;;
-    h )
-      echo "Usage: ./install.sh [OPTIONS]"
-      echo "Options:"
-      echo "  -d    Specify block device"
-      echo "  -h    Display this help message"
-      echo "  -a    WHY ARE YOU HERER THIS IS DEV THINGS????"
-      ;;
-    \? )
-      echo "Invalid option: $OPTARG"
-      ;;
-  esac
-done
+rm /etc/apt/sources.list
+echo "deb http://deb.debian.org/debian/ stable main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ stable main contrib non-free non-free-firmware
+deb http://security.debian.org/debian-security stable-security main contrib non-free non-free-firmware
+deb-src http://security.debian.org/debian-security stable-security main contrib non-free non-free-firmware
+deb http://deb.debian.org/debian/ stable-updates main contrib non-free non-free-firmware
+deb-src http://deb.debian.org/debian/ stable-updates main contrib non-free non-free-firmware" | tee /etc/apt/sources.list
+dpkg --add-architecture i386
+apt update
+apt install mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
+apt install steam-installer
