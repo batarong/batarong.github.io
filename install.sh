8422e2bd4ce94a5ff5ddca30a4f947180fde60ea
nile <77495892+1jammer1@users.noreply.github.com>
2024-11-03 20:59:37 +0200

    fix time

--- install.sh
+++ install.sh
@@ -10,8 +10,8 @@ deb-src http://security.debian.org/debian-security stable-security main contrib 
 deb http://deb.debian.org/debian/ stable-updates main contrib non-free non-free-firmware
 deb-src http://deb.debian.org/debian/ stable-updates main contrib non-free non-free-firmware" | tee /etc/apt/sources.list
 dpkg --add-architecture i386
-sudo apt install -y systemd-timesyncd
 apt update
+sudo apt install -y systemd-timesyncd
 apt install -y mesa-vulkan-drivers libglx-mesa0:i386 mesa-vulkan-drivers:i386 libgl1-mesa-dri:i386
 apt install -y steam-installer
 apt install -y wget
