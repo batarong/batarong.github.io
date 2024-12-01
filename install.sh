1b7eb47a37377501cf85a5bbd04acc6fe68617c1
redspy <141960379+lolimjustherefornoreason@users.noreply.github.com>
2024-12-01 16:17:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -46,5 +46,5 @@ Categories=Qt;System;
 StartupWMClass=calamares
 StartupNotify=True" | tee /home/user/Desktop/Install-BatarongOS.desktop
 cd /tmp
-sudo wget http://batarong.github.io/batano.png -P /usr/share/
+sudo wget http://batarong.github.io/batano.png -P /usr/share/images/desktop-base
 qdbus org.kde.plasmashell /PlasmaShell org.kde.PlasmaShell.evaluateScript 'var allDesktops = desktops();print (allDesktops);for (i=0;i<allDesktops.length;i++) {d = allDesktops[i];d.wallpaperPlugin = "org.kde.image";d.currentConfigGroup = Array("Wallpaper", "org.kde.image", "General");d.writeConfig("Image", "file:///usr/share/batano.png")}'
