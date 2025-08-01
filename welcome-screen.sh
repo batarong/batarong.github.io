14fdd1a5459800ef6655cdcd666c8725dc49de8e
Timmy <141960379+timmyofficial@users.noreply.github.com>
2025-08-01 12:14:57 +0300

    Update welcome-screen.sh

--- welcome-screen.sh
+++ welcome-screen.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+if [ ! -f "$HOME/.first_login_done" ]; then
+  clear
+  echo "======================================="
+  echo "        Welcome to BatarongOS!!!       "
+  echo "======================================="
+  echo "Feel free to check out the Batarong Game XD"
+  echo "Github: https://github.com/batarong"
+  echo "- Based on Debian"
+  echo "- Modified by Mam man dev and many others"
+  echo "Idk what else to say"
+  echo "So."
+  echo "Enjoy Batarong!"
+  touch "$HOME/.first_login_done"
+fi
